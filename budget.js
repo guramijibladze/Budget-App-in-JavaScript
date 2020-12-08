b0eec204078534996c2aa3dcb85f7902cb2f8280
guramijibladze <guramijibladze@gmail.com>
2020-12-08 22:29:05 +0400

    up

---
+++ budget.js
@@ -0,0 +1,10 @@
+// SELECT ELEMENTS
+
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".icome-total");
+const outcomeTotalEl = document.querySelector(".outcome-total");
+const balanceEl = document.querySelector(".balance .value");
+const balanceEl = document.querySelector(".balance .value");
+const balanceEl = document.querySelector(".balance .value");
+const balanceEl = document.querySelector(".balance .value");
+const balanceEl = document.querySelector(".balance .value");
